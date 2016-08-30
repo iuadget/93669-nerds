0b4634bcb773e10a14041386495054f44775320c
iuadget <iuadget@gmail.com>
2016-08-30 19:47:43 +0300

    добавил модальное окно

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var popBtn = document.querySelector(".btn-pop-up");
+var popUp = document.querySelector(".pop-up");
+var popClose = document.querySelector(".pop-up-close");
+var login = popUp.querySelector("[name=name");
+
+popBtn.addEventListener("click", function(event) {
+  event.preventDefault();
+  popUp.classList.add("pop-up-opened");
+  login.focus();
+});
+popClose.addEventListener("click", function(event) {
+  event.preventDefault();
+  popUp.classList.remove("pop-up-opened");
+});
+window.addEventListener("keydown", function(event) {
+  if (event.keyCode == 27) {
+    if (popUp.classList.contains("pop-up-opened")) {
+      popUp.classList.remove("pop-up-opened");
+    }
+  }
+});
